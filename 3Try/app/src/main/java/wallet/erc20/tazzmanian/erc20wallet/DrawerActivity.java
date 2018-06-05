0e2b9546ab86dc573bc8a6cb10859e59b9e6d69c
Tazzmanian <banskaliqtabg@yahoo.com>
2018-06-05 19:31:46 +0300

    change drawer items and add toolbar

--- 3Try/app/src/main/java/wallet/erc20/tazzmanian/erc20wallet/DrawerActivity.java
+++ 3Try/app/src/main/java/wallet/erc20/tazzmanian/erc20wallet/DrawerActivity.java
@@ -80,17 +80,11 @@ public class DrawerActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_accounts) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_contracts) {
 
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_server) {
 
         }
 
--- 3Try/app/src/main/java/wallet/erc20/tazzmanian/erc20wallet/MainActivity.java
+++ 3Try/app/src/main/java/wallet/erc20/tazzmanian/erc20wallet/MainActivity.java
@@ -83,17 +83,11 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_accounts) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_contracts) {
 
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_server) {
 
         }
 
