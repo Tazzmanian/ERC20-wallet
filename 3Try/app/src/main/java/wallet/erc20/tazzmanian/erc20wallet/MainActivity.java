792f9a6bd08c2e9226ca11a0f765dbdf4cb7894b
Tazzmanian <banskaliqtabg@yahoo.com>
2018-06-05 21:38:27 +0300

    remove more_optiron from the toolbar

--- 3Try/app/src/main/java/wallet/erc20/tazzmanian/erc20wallet/MainActivity.java
+++ 3Try/app/src/main/java/wallet/erc20/tazzmanian/erc20wallet/MainActivity.java
@@ -75,7 +75,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.drawer, menu);
+//        getMenuInflater().inflate(R.menu.drawer, menu);
         return true;
     }
 
