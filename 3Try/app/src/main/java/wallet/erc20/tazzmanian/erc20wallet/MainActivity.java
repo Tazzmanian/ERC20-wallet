d02f0d3b9d64dcba040f2987dce30389a75a6964
Tazzmanian <banskaliqtabg@yahoo.com>
2018-06-07 22:59:00 +0300

    .. TODO: change the bottom navigation onBack

--- 3Try/app/src/main/java/wallet/erc20/tazzmanian/erc20wallet/MainActivity.java
+++ 3Try/app/src/main/java/wallet/erc20/tazzmanian/erc20wallet/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -19,12 +20,33 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
+import java.util.Stack;
+
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener,
         TransactionsFragment.OnFragmentInteractionListener, SendFragment.OnFragmentInteractionListener,
         UserInfoFragment.OnFragmentInteractionListener, AccountsFragment.OnFragmentInteractionListener,
         ContractFragment.OnFragmentInteractionListener, ServerFragment.OnFragmentInteractionListener {
 
     private TextView mTextMessage;
+
+    private int lastMenuId = 0;
+    private int lastFrameId = 0;
+
+    private class Frame {
+        int frameId;
+        int menuId;
+        int menuItemId;
+        Fragment fragment;
+
+        Frame(int f, int m, int i, Fragment fr) {
+            frameId = f;
+            menuId = m;
+            menuItemId = i;
+            fragment = fr;
+        }
+    }
+
+    private Stack<Frame> frameStack = new Stack<>();
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -36,16 +58,19 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 case R.id.navigation_history:
                     mTextMessage.setText(R.string.title_history);
                     loadFragment(new TransactionsFragment());
+                    loadBottomNavigation(R.id.navigation_history, R.menu.navigation);
                     flag = true;
                     break;
                 case R.id.navigation_send:
                     mTextMessage.setText(R.string.title_send);
                     loadFragment(new SendFragment());
+                    loadBottomNavigation(R.id.navigation_send, R.menu.navigation);
                     flag = true;
                     break;
                 case R.id.navigation_address:
                     mTextMessage.setText(R.string.title_address);
                     loadFragment(new UserInfoFragment());
+                    loadBottomNavigation(R.id.navigation_address, R.menu.navigation);
                     flag = true;
                     break;
             }
@@ -58,6 +83,11 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        loadBottomNavigation(R.id.navigation)
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.add(R.id.main_frame_layout, new TransactionsFragment());
+        fragmentTransaction.commit();
 
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
@@ -81,9 +111,17 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
         }
+//        else {
+//            super.onBackPressed();
+//        }
+
+        FragmentManager fm = getSupportFragmentManager();
+        if(fm.getBackStackEntryCount() > 0){
+            fm.popBackStack();
+            return;
+        }
+        super.onBackPressed();
     }
 
     @Override
@@ -112,24 +150,29 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
+
         boolean flag = false;
         switch (item.getItemId()) {
             case R.id.nav_accounts:
                 loadFragment(new AccountsFragment());
+                loadBottomNavigation(R.id.nav_accounts, R.menu.account_manage_menu);
                 flag = true;
                 break;
             case R.id.nav_contracts:
                 loadFragment(new ContractFragment());
+//                loadBottomNavigation(R.id.nav_contracts, null);
                 flag = true;
                 break;
             case R.id.nav_server:
                 loadFragment(new ServerFragment());
+//                loadBottomNavigation(R.id.nav_server);
                 flag = true;
                 break;
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
+
         return flag;
     }
 
@@ -138,6 +181,53 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         fragmentTransaction.replace(R.id.main_frame_layout, fragment);
         fragmentTransaction.addToBackStack(null);
         fragmentTransaction.commit();
+    }
+
+    private void addFragment(Fragment fragment, String name) {
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.container, fragment, name)
+                .addToBackStack(name).commit();
+    }
+
+    private void replaceFragment(Fragment fragment, String name) {
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.container, fragment, name)
+                .addToBackStack(name).commit();
+    }
+
+    private void loadBottomNavigation(int frameId, int menuId) {
+        if(lastMenuId == 0) {
+            lastMenuId = menuId;
+            return;
+        }
+
+        if(lastMenuId == menuId) {
+            return;
+        }
+
+        switch (frameId) {
+            case R.id.navigation_history:
+            case R.id.navigation_send:
+            case R.id.navigation_address:
+                loadMenu(menuId);
+                break;
+            case R.id.nav_accounts:
+                loadMenu(menuId);
+                break;
+            case R.id.nav_contracts:
+                loadMenu(menuId);
+                break;
+            case R.id.nav_server:
+                loadMenu(menuId);
+                break;
+        }
+        lastMenuId = frameId;
+    }
+
+    private void loadMenu(int menuId) {
+        BottomNavigationView navigationView = (BottomNavigationView) findViewById(R.id.navigation);
+        navigationView.getMenu().clear();
+        navigationView.inflateMenu(menuId);
     }
 
     @Override
