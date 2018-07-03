d9cca783e23b869f1a854cf72ca25a9224a8c721
Tazzmanian <banskaliqtabg@yahoo.com>
2018-07-03 21:50:21 +0300

    .

---
+++ Web3jWrapper/src/test/java/com/tazz/web3jWrapper/Web3jWrapperApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tazz.web3jWrapper;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Web3jWrapperApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
