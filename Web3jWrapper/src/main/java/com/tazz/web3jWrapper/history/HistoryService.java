2d5d4f63367b953beb6a44be77e114f58fc55c85
Tazzmanian <banskaliqtabg@yahoo.com>
2018-09-07 14:44:46 +0300

    process contracts creation and transfer function

--- Web3jWrapper/src/main/java/com/tazz/web3jWrapper/history/HistoryService.java
+++ Web3jWrapper/src/main/java/com/tazz/web3jWrapper/history/HistoryService.java
@@ -5,7 +5,11 @@
  */
 package com.tazz.web3jWrapper.history;
 
+import com.tazz.web3jWrapper.contracts.ContractService;
+import com.tazz.web3jWrapper.contracts.ERC20Wrapper;
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
@@ -13,13 +17,27 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.web3j.abi.TypeDecoder;
+import org.web3j.abi.datatypes.Address;
+import org.web3j.abi.datatypes.generated.Uint256;
 import org.web3j.protocol.Web3j;
 import org.web3j.protocol.core.DefaultBlockParameter;
 import org.web3j.protocol.core.DefaultBlockParameterName;
 import org.web3j.protocol.core.Request;
 import org.web3j.protocol.core.methods.response.EthBlock;
+import org.web3j.protocol.core.methods.response.EthGetTransactionReceipt;
+import org.web3j.protocol.core.methods.response.Log;
 import org.web3j.protocol.core.methods.response.Transaction;
 import org.web3j.protocol.http.HttpService;
+import org.web3j.tx.ClientTransactionManager;
+import org.web3j.tx.Transfer;
+import org.web3j.tx.gas.DefaultGasProvider;
+import rx.Completable;
+import rx.Observable;
+import rx.Subscription;
+import rx.functions.Action1;
+import rx.observables.BlockingObservable;
+import rx.subjects.PublishSubject;
 
 /**
  *
@@ -29,149 +47,165 @@ import org.web3j.protocol.http.HttpService;
 @Service
 public class HistoryService {
 
+    private final ContractService contractService;
+
+    public HistoryService(ContractService contractService) {
+        this.contractService = contractService;
+    }
+
     public List<HistoryResponseDTO> getHistory(HistoryRequestDTO dto) {
         List<HistoryResponseDTO> res = new ArrayList<>();
+        Observable ob = null;
+        Subscription sub = null;
         try {
             Web3j web3 = Web3j.build(new HttpService(dto.getNetwork()));
             DefaultBlockParameter start = DefaultBlockParameter.valueOf(dto.getBlockNumber());
-            Request<?, EthBlock> request = web3.ethGetBlockByNumber(DefaultBlockParameterName.LATEST, false);
-            DefaultBlockParameter end = DefaultBlockParameter.valueOf(web3.ethBlockNumber().send().getBlockNumber());
-            
-            web3.catchUpToLatestTransactionObservable(start).subscribe(tx -> {
-                log.info("===============================");
-                log.info("block hash " + tx.getBlockHash());
-                log.info("block number row " + tx.getBlockNumberRaw());
-                log.info("creates " + tx.getCreates());
-                log.info("from " + tx.getFrom());
-                log.info("gas price raw " + tx.getGasPriceRaw());
-                log.info("gas row " + tx.getGasRaw());
-                log.info("hash " + tx.getHash());
-                log.info("input " + tx.getInput());
-                log.info("nonce row " + tx.getNonceRaw());
-                log.info("public key " + tx.getPublicKey());
-                log.info("r " + tx.getR());
-                log.info("raw " + tx.getRaw());
-                log.info("s " + tx.getS());
-                log.info("to " + tx.getTo());
-                log.info("tx index raw " + tx.getTransactionIndexRaw());
-                log.info("value raw " + tx.getValueRaw());
-                log.info("block number " + tx.getBlockNumber().toString());
-                log.info("chain id " + tx.getChainId().toString());
-                log.info("gas " + tx.getGas().toString());
-                log.info("gas price " + tx.getGasPrice().toString());
-                log.info("nonce " + tx.getNonce().toString());
-                log.info("v " + BigInteger.valueOf(tx.getV()).toString());
-                log.info("value " + tx.getValue().toString());
+
+            log.info("Number of active threads from the given thread 1: " + Thread.activeCount());
+
+            ob = web3.catchUpToLatestTransactionObservable(start).asObservable();
+
+            ob.toBlocking().forEach(new Action1<Transaction>() {
+                @Override
+                public void call(Transaction tx) {
+                    log.info("Number of active threads from the given thread 2: " + Thread.activeCount());
+                    if (tx.getNonce().compareTo(dto.getNonce()) > 0) {
+                        HistoryResponseDTO r = new HistoryResponseDTO();
+                        r.setBlockNumber(tx.getBlockNumber().toString());
+                        r.setFrom(tx.getFrom());
+                        r.setTo(tx.getTo());
+                        r.setNonce(tx.getNonce().toString());
+                        r.setNetwork(dto.getNetwork());
+                        r.setAmount(tx.getValue().toString());
+                        r.setContract(tx.getTo());
+                        r.setTxHash(tx.getHash());
+                        r.setContract("");
+                        r.setCurruncy("ETH");
+                        res.add(r);
+                        log.info("===============================");
+                        log.info("from " + tx.getFrom());
+                        log.info("hash " + tx.getHash());
+                        log.info("input " + tx.getInput());
+                        log.info("to " + tx.getTo());
+                        log.info("block number " + tx.getBlockNumber().toString());
+                        log.info("nonce " + tx.getNonce().toString());
+                        log.info("value " + tx.getValue().toString());
+                        if (r.getTo().equals("0x0")) {
+                            createdContract(r, web3);
+                        } else if ((!dto.getAddress().equals(r.getFrom()) || dto.getAddress().equals(r.getTo()))
+                                && (dto.getAddress().equals(r.getFrom()) || !dto.getAddress().equals(r.getTo()))) {
+                            transfert(tx.getInput(), r, web3);
+                        }
+                    }
+                    log.info("Number of active threads from the given thread 3: " + Thread.activeCount());
+                }
             });
-            
-//            web3.replayBlocksObservable(start, end, false).subscribe(b -> {
-//                log.info("===============================");
-//                log.info("rpc " + b.getJsonrpc());
-//                log.info("raw resp " + b.getRawResponse());
-//                log.info("raw id " + b.getId());
-//                log.info("error " + b.getError().getData());
-//                log.info("error " + b.getError().getMessage());
-//                log.info("error " + b.getError().getCode());
-//                log.info("autor " + b.getBlock().getAuthor());
-//                log.info("diff raw " + b.getBlock().getDifficultyRaw());
-//                log.info("get extra data " + b.getBlock().getExtraData());
-//                log.info("gas limit raw " + b.getBlock().getGasLimitRaw());
-//                log.info("gas used raw " + b.getBlock().getGasUsedRaw());
-//                log.info("hash " + b.getBlock().getHash());
-//                log.info("bloom " + b.getBlock().getLogsBloom());
-//                log.info("miner " + b.getBlock().getMiner());
-//                log.info("mix hash " + b.getBlock().getMixHash());
-//                log.info("nonce raw " + b.getBlock().getNonceRaw());
-//                log.info("number raw " + b.getBlock().getNumberRaw());
-//                log.info("parent " + b.getBlock().getParentHash());
-//                log.info("receopts " + b.getBlock().getReceiptsRoot());
-//                log.info("sha3 " + b.getBlock().getSha3Uncles());
-//                log.info("size raw " + b.getBlock().getSizeRaw());
-//                log.info("state root " + b.getBlock().getStateRoot());
-//                log.info("timeraw " + b.getBlock().getTimestampRaw());
-//                log.info("diff raw " + b.getBlock().getTotalDifficultyRaw());
-//                log.info("tranroot " + b.getBlock().getTransactionsRoot());
-//                log.info("diffic " + b.getBlock().getDifficulty());
-//                log.info("gas limit " + b.getBlock().getGasLimit());
-//                log.info("gas used " + b.getBlock().getGasUsed());
-//                log.info("nonce " + b.getBlock().getNonce());
-//                log.info("number " + b.getBlock().getNumber());
-//                log.info("fields " + b.getBlock().getSealFields());
-//                log.info("time " + b.getBlock().getTimestamp());
-//                log.info("diff " + b.getBlock().getTotalDifficulty());
-//                log.info("uncle " + b.getBlock().getUncles());
-//                log.info("size " + b.getBlock().getTransactions().size());
-//                b.getBlock().getTransactions().forEach(x -> {
-//                    log.info("x " + x.get().toString());
-//                });
-//                
-//                log.info("-------- ");
-//                log.info("autor " + b.getResult().getAuthor());
-//                log.info("diff raw " + b.getResult().getDifficultyRaw());
-//                log.info("get extra data " + b.getResult().getExtraData());
-//                log.info("gas limit raw " + b.getResult().getGasLimitRaw());
-//                log.info("gas used raw " + b.getResult().getGasUsedRaw());
-//                log.info("hash " + b.getResult().getHash());
-//                log.info("bloom " + b.getResult().getLogsBloom());
-//                log.info("miner " + b.getResult().getMiner());
-//                log.info("mix hash " + b.getResult().getMixHash());
-//                log.info("nonce raw " + b.getResult().getNonceRaw());
-//                log.info("number raw " + b.getResult().getNumberRaw());
-//                log.info("parent " + b.getResult().getParentHash());
-//                log.info("receopts " + b.getResult().getReceiptsRoot());
-//                log.info("sha3 " + b.getResult().getSha3Uncles());
-//                log.info("size raw " + b.getResult().getSizeRaw());
-//                log.info("state root " + b.getResult().getStateRoot());
-//                log.info("timeraw " + b.getResult().getTimestampRaw());
-//                log.info("diff raw " + b.getResult().getTotalDifficultyRaw());
-//                log.info("tranroot " + b.getResult().getTransactionsRoot());
-//                log.info("diffic " + b.getResult().getDifficulty());
-//                log.info("gas limit " + b.getResult().getGasLimit());
-//                log.info("gas used " + b.getResult().getGasUsed());
-//                log.info("nonce " + b.getResult().getNonce());
-//                log.info("number " + b.getResult().getNumber());
-//                log.info("fields " + b.getResult().getSealFields());
-//                log.info("time " + b.getResult().getTimestamp());
-//                log.info("diff " + b.getResult().getTotalDifficulty());
-//                log.info("uncle " + b.getResult().getUncles());
-//                log.info("size " + b.getResult().getTransactions().size());
-//                b.getResult().getTransactions().forEach(x -> {
-//                    log.info("x " + x.get().toString());
-//                });
-//            });
-//
-//            web3.replayTransactionsObservable(start, end).subscribe(tx -> {
-//                log.info("===============================");
-//                log.info("block hash " + tx.getBlockHash());
-//                log.info("block number row " + tx.getBlockNumberRaw());
-//                log.info("creates " + tx.getCreates());
-//                log.info("from " + tx.getFrom());
-//                log.info("gas price raw " + tx.getGasPriceRaw());
-//                log.info("gas row " + tx.getGasRaw());
-//                log.info("hash " + tx.getHash());
-//                log.info("input " + tx.getInput());
-//                log.info("nonce row " + tx.getNonceRaw());
-//                log.info("public key " + tx.getPublicKey());
-//                log.info("r " + tx.getR());
-//                log.info("raw " + tx.getRaw());
-//                log.info("s " + tx.getS());
-//                log.info("to " + tx.getTo());
-//                log.info("tx index raw " + tx.getTransactionIndexRaw());
-//                log.info("value raw " + tx.getValueRaw());
-//                log.info("block number " + tx.getBlockNumber().toString());
-//                log.info("chain id " + tx.getChainId().toString());
-//                log.info("gas " + tx.getGas().toString());
-//                log.info("gas price " + tx.getGasPrice().toString());
-//                log.info("nonce " + tx.getNonce().toString());
-//                log.info("v " + BigInteger.valueOf(tx.getV()).toString());
-//                log.info("value " + tx.getValue().toString());
-//            });
-            
-            
+
         } catch (Exception ex) {
             log.info("History: " + ex.getMessage());
         }
+
+        ob.subscribe().unsubscribe();
+        log.info("Number of active threads from the given thread 4: " + Thread.activeCount());
         return res;
     }
-    
+
+    private void transfert(String input, HistoryResponseDTO res, Web3j web3) {
+        Method refMethod = null;
+        try {
+            if (input.isEmpty()) {
+                return;
+            }
+//            String inputData = "0xa9059cbb0000000000000000000000005c5212ed85cc957c6b656d209a7be8812ab00e330000000000000000000000000000000000000000000000008d8dadf544fc0000";
+            String method = input.substring(0, 10);
+            System.out.println(method);
+            log.info("method: " + method);
+            String to = input.substring(10, 74);
+            String value = input.substring(74);
+            refMethod = TypeDecoder.class.getDeclaredMethod("decode", String.class, int.class, Class.class);
+            refMethod.setAccessible(true);
+            Address address = (Address) refMethod.invoke(null, to, 0, Address.class);
+            System.out.println(address.toString());
+            res.setContract(res.getTo());
+            res.setTo(address.toString());
+            log.info("address: " + address.toString());
+            Uint256 amount = (Uint256) refMethod.invoke(null, value, 0, Uint256.class);
+            System.out.println(amount.getValue());
+            res.setAmount(amount.getValue().toString());
+            log.info("amount: " + amount.getValue());
+
+            ClientTransactionManager clientManager = new ClientTransactionManager(web3, res.getTo());
+            ERC20Wrapper contract = ERC20Wrapper.load(res.getContract(), web3, clientManager, DefaultGasProvider.GAS_PRICE, Transfer.GAS_LIMIT);
+
+            res.setCurruncy(contract.symbol().send());
+        } catch (NoSuchMethodException ex) {
+            Logger.getLogger(HistoryService.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SecurityException ex) {
+            Logger.getLogger(HistoryService.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            Logger.getLogger(HistoryService.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IllegalArgumentException ex) {
+            Logger.getLogger(HistoryService.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (InvocationTargetException ex) {
+            Logger.getLogger(HistoryService.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (Exception ex) {
+            Logger.getLogger(HistoryService.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+//            refMethod.close();
+        }
+    }
+
+    private void createdContract(HistoryResponseDTO dto, Web3j web3) {
+        Method refMethod = null;
+        try {
+            EthGetTransactionReceipt rec = web3.ethGetTransactionReceipt(dto.getTxHash()).send();
+            List<Log> logs = rec.getResult().getLogs();
+            dto.setContract(logs.get(0).getAddress());
+            refMethod = TypeDecoder.class.getDeclaredMethod("decode", String.class, int.class, Class.class);
+            refMethod.setAccessible(true);
+            Uint256 amount = (Uint256) refMethod.invoke(null, logs.get(0).getData(), 0, Uint256.class);
+            System.out.println(amount.getValue());
+            dto.setAmount(amount.getValue().toString());
+            
+            ClientTransactionManager clientManager = new ClientTransactionManager(web3, dto.getFrom());
+            ERC20Wrapper contract = ERC20Wrapper.load(dto.getContract(), web3, clientManager, DefaultGasProvider.GAS_PRICE, Transfer.GAS_LIMIT);
+
+            dto.setCurruncy(contract.symbol().send());
+            
+            logs.forEach(l -> {
+                log.info("++++++");
+                log.info(l.getAddress());
+                log.info(l.getBlockHash());
+                log.info(l.getBlockNumberRaw());
+                log.info(l.getData());
+                log.info(l.getLogIndexRaw());
+                log.info(l.getTransactionHash());
+                log.info(l.getTransactionIndexRaw());
+                log.info(l.getType());
+                log.info(l.getBlockNumber().toString());
+                log.info(l.getLogIndex().toString());
+                log.info(l.getTransactionIndex().toString());
+                log.info("----");
+                l.getTopics().forEach(t -> {
+                    log.info(t.toString());
+                });
+                log.info("++++++");
+            });
+        } catch (IOException ex) {
+            Logger.getLogger(HistoryService.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (NoSuchMethodException ex) {
+            Logger.getLogger(HistoryService.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SecurityException ex) {
+            Logger.getLogger(HistoryService.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            Logger.getLogger(HistoryService.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IllegalArgumentException ex) {
+            Logger.getLogger(HistoryService.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (InvocationTargetException ex) {
+            Logger.getLogger(HistoryService.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (Exception ex) {
+            Logger.getLogger(HistoryService.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
 }
